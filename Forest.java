0172ddd8e608a6f73045ce8e8324e0ea4ddaa7a6
Momo <mlloydtucker@smith.edu>
2023-05-04 00:22:36 -0400

    more fixes

--- Forest.java
+++ Forest.java
@@ -5,12 +5,12 @@ public class Forest extends Location{
         super(description, 10.0, false);
         this.description = "you are in a large forest with big trees in the middle is a large twisting tree lots of branches that would take a while to climb. It has fruit hanging from the branches";
         Plant fig = new Plant("fallen fig", "some of the fruit has dropped from the tree. they are rich in calcium and can help to restore a shell", false, false, true);
-        Plant hemlock = new Plant("hemlock", "a small tree with little white flowers", true, true, false);
+        Plant hemlock = new Plant("hemlock", "also a small tree with little white flowers growing near by", true, true, false);
         this.addPlant(fig);
         this.addPlant(hemlock);
     }
     public void description() {
-        System.out.println(this.description + "the humidity is " + this.humidity);
+        System.out.println(this.description + " the humidity is " + this.humidity);
         System.out.println("you see the following plants:");
         Set<String> setOfKeys = this.plants.keySet();
         for(String s : setOfKeys) {
